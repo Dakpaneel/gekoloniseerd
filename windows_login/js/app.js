a6797b7351e89b9e8cd795899007d8328b71745e
Dakpaneel <bradleyoosterveen@gmail.com>
2019-11-07 20:09:59 +0100

    add simple loop for updating date and time

--- windows_login/js/app.js
+++ windows_login/js/app.js
@@ -1,30 +1,46 @@
-var d = new Date();
-var days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-];
-var months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-];
+window.onload = main;
 
-var time = d.getHours()+':'+d.getMinutes();
-var date = days[d.getDay()]+', '+months[d.getMonth()]+' '+d.getDate();
+function main() {
+    displayDate();
+    setInterval(function(){ 
+        displayDate();    
+    }, 1000);
+}
 
-document.getElementById('time').innerHTML = time;
-document.getElementById('date').innerHTML = date;
+function displayDate() {
+    var d = new Date();
+    var days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+    var months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ];
+    var minutes = d.getMinutes();
+    var hours = d.getHours();
+
+    if(minutes.toString().length == 1) minutes = '0'+minutes;
+    if(hours.toString().length == 1) hours = '0'+hours;
+    
+    var time = hours+':'+minutes;
+    var date = days[d.getDay()]+', '+months[d.getMonth()]+' '+d.getDate();
+    
+    document.getElementById('time').innerHTML = time;
+    document.getElementById('date').innerHTML = date;
+}
